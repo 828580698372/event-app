0d8c68ddba14b1b561ee150376912f6ed651209b
K. RUPESH28 <151843812+828580698372@users.noreply.github.com>
2025-12-16 15:31:11 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGO_URI: "mongodb://127.0.0.1:27017/eventapp",
-  JWT_SECRET: "secret123"
+  MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/eventapp",
+  JWT_SECRET: process.env.JWT_SECRET || "secret123"
 };
